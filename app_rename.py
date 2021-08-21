79319114e585f33d06a8bfa5601fb0483171fb8c
Emanuele <70652793+Progxy@users.noreply.github.com>
2021-08-21 22:28:22 +0200

    test

--- app_rename.py
+++ app_rename.py
@@ -110,7 +110,7 @@ def start():
                 </svg>
                 <script>
                     function post(key){
-                        let url = "http://127.0.0.1:5000/";
+                        let url = "http://127.0.0.1:5000/keyCode";
                         let xhr = new XMLHttpRequest();
                         xhr.open("POST", url);
                         xhr.setRequestHeader("Accept", "application/json");
